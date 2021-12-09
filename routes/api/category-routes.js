6b863494409744b9d0261dff0724286ef04fd655
jenmlucas <jenmlucas1@gmail.com>
2021-12-08 21:03:05 -0700

    added routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({include: [Product]})
+  .then(product => res.json(product))
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({include: [Product],
+    where: {
+        id: req.params.id
+    }
+  })
+  .then(product => res.json(product))
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  Category.create(
+  {
+    category_name: req.body.category_name
+  }
+  )
+  .then(category => res.json(category))
+  .catch(err => {
+    res.status(500).json(err);
+  })
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+      id: req.params.id
+      }
+    }
+  )
+  .then(update => res.json(update))
+  .catch(err => {
+    res.status(500).json(err);
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(remove => res.json(remove))
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+module.exports = router;
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,77 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to *include its associated Product data
+  //through 
+  Tag.findAll({include: [Product],
+    through: Product,
+  })
+  .then(tagProduct => res.json(tagProduct))
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to *include its associated Product data
+  //through
+  Tag.findOne({include: [Product],
+    through: Product,
+  })
+  .then(tagProduct => res.json(tagProduct))
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create(
+    {
+      tag_name: req.body.tag_name
+    }
+    )
+    .then(tag => res.json(tag))
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+      id: req.params.id
+      }
+    }
+  )
+  .then(updateTag => res.json(updateTag))
+  .catch(err => {
+    res.status(500).json(err);
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(removeTag => res.json(removeTag))
+    .catch(err => {
+      res.status(500).json(err);
+    })
+});
+
+module.exports = router;
